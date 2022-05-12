2e8eb836386e02b78cc17cc8c8ac67de65abb29d
kevin910731 <100269237+kevin910731@users.noreply.github.com>
2022-05-12 14:07:38 +0800

    Add files via upload

---
+++ qz8/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MultiButton from  './MultiButton';
+import HelloCGU from './HelloCGU';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {HelloCGU()}
+      </div>
+      <div>
+        {MultiButton(10)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
