15126e02fc368dc83fe2a19a209c965a7bcba757
kevin910731 <100269237+kevin910731@users.noreply.github.com>
2022-05-18 19:39:38 +0800

    Add files via upload

---
+++ hw8/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SignInSide from './SignInSide';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {SignInSide()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
