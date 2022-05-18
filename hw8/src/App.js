8115714a677b09aafd1e8d01af9756e1663f0b77
kevin910731 <100269237+kevin910731@users.noreply.github.com>
2022-05-18 16:43:02 +0800

    Add files via upload

---
+++ hw8/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LogIn from './hw8';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {LogIn()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
