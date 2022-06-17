c4356c7af03c2b2531ec867fb71842a4787ac24c
kevin910731 <100269237+kevin910731@users.noreply.github.com>
2022-06-17 19:31:04 +0800

    Add files via upload

---
+++ hw1/show_main.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA_COOL</title>
+    <link rel="stylesheet" href="show.css">
+    <script src="show.js"></script>
+    <script src="show_main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    <div class="all">
+        <div class="menu">
+            <a href="show_main.php"><img src="LOTUS.jpg" width="200" alt=""></a>
+            <div class="list">
+                <br>
+                <input type="button" class="menu_bnt" name="search_funeral" value="會館搜尋" onclick="show_funeral();"/>
+                <br>
+                <br>
+                <input type="button" class="menu_bnt" name="search_cemetery" value="墓園搜尋" onclick="show_cemetery();"/>
+                <br>
+                <br>
+                <input type="button" class="menu_bnt" name="search_paper" value="金紙搜尋" onclick="show_paper();"/>
+                <br>
+                <br>
+                <input type="button" class="menu_bnt" name="search_fruit" value="水果搜尋" onclick="show_fruit();"/>
+            </div>
+            <div class="info">
+                <br><a id="go_manage" href="manage_login.php" class="neon">DATA_COOL</a>
+                <br>CSIE@CGU
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="subtitle"><h1>PROLOGUE</h1></div>
+            <div class="content_frame">
+                <br>
+                <h3>Since the epidemic has swept the world in recent years.</h3>
+                <h3>The number of deaths is also higher than before.</h3>
+                <h3>The number of browsing the graves page has increased.</h3>
+                <br>
+                <h3>However, the information on those websites are very fragmented.</h3>
+                <h3>It often takes a lot of time to search for related matters.</h3>
+                <br>
+                <h3>Therefore, we have developed a convenient and quick search for those who need it.</h3>
+                <h3>At present, we only use the area within municipality as the database of this website.</h3>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+            </div>
+            <h5 class="cpr">Copyright © 2022 DATA_COOL.</h5>
+        </div>
+
+        
+
+    </div>
+
+</body>
+</html>
